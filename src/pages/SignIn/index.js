53d0410e05cf6281fddedeb60b0b50292259b0ef
ViniciusNyari <viniciusnyari@gmail.com>
2020-10-10 12:08:25 -0300

    Configurando background

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '../../components/Background';
 
 const SignIn = () => {
-  return <Text>SignIn</Text>;
+  return <Background>
+    <Text>SignIn</Text>
+  </Background>
+  ;
 }
 
 export default SignIn;
