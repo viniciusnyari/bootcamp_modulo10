62b247aba3c29f2b1cbc31fef00cb0115b3f226a
ViniciusNyari <viniciusnyari@gmail.com>
2020-10-10 11:40:36 -0300

    Ambiente inicial

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const SingUp = () => {
+  return <Text>SingUp</Text>;
+}
+
+export default SingUp;
