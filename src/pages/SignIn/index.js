6c3a3428eab33bb9e86d3120c2b6be967ed73d86
ViniciusNyari <viniciusnyari@gmail.com>
2020-10-10 15:26:57 -0300

    Input & Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import Background from '../../components/Background';
 
 const SignIn = () => {
   return <Background>
     <Text>SignIn</Text>
+    <Input 
+      style={{marginTop:30}} 
+      icon="call" 
+      placeholder="Digite seu nome!" 
+      />
+      <Button loading={false}>Entrar</Button>
   </Background>
   ;
 }
