cfe7a075df7bba14bd14357c611d971f37d0c730
ViniciusNyari <viniciusnyari@gmail.com>
2020-10-15 08:56:37 -0300

    correção de problema com reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,14 +1,21 @@
-import Reactotron from 'reactotron-react-native';
+//import Reactotron from 'reactotron-react-native';
+//import AsyncStorage from '@react-native-community/async-storage';
+//import { AsyncStorage } from 'react-native'
+import Reactotron, { asyncStorage } from 'reactotron-react-native'
 import {reactotronRedux} from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
 if( __DEV__){  
-  const tron = Reactotron.configure({
-    host: '192.168.0.4', // my current wifi local ip in mac
-    port: 9090,
-  })
+  //const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+  const tron = Reactotron
+   .configure({
+     host: '192.168.15.12', // my current wifi local ip in mac
+     port: 9090,
+   })
+  //.configure()
   .useReactNative()
   .use(reactotronRedux())
+  .use(asyncStorage())
   .use(reactotronSaga())
   .connect();
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text, 
-  StatusBar,
-} from 'react-native';
+import { PersistGate} from 'redux-persist/integration/react';
+import { Provider} from 'react-redux';
+import { StatusBar} from 'react-native';
 
 import './config/ReactotronConfig';
+import { store, persistor} from './store';
+
 import Routes from './routes';
 
 export default function App() {
-  return <>
-         <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-         <Routes/>
-         </>;
+  return <Provider store={store}>
+            <PersistGate persistor={persistor}>
+              <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+              <Routes/>
+            </PersistGate>
+         </Provider>;
 }
