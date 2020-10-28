cdf7b9c3f8f7eae2b82ad94e262cccbe1c388dd0
ViniciusNyari <viniciusnyari@gmail.com>
2020-10-28 08:54:28 -0300

    Ajustes do Redux e Roteamento

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState} from 'react';
 import { Image } from 'react-native';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 
 import logo from '../../assets/logo.png';
 import Background from '../../components/Background';
@@ -14,6 +14,8 @@ export default function SignIn({navigation}) {
 
   const [ email, setEmail] = useState('');
   const [ password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit(){    
     dispatch(signInRequest(email, password));
@@ -47,7 +49,7 @@ export default function SignIn({navigation}) {
             onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>          
+          <SubmitButton loading={loading} onPress={handleSubmit}>Acessar</SubmitButton>          
         </Form>
 
         <SignLink onPress={()=> navigation.navigate('SignUp')}>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState} from 'react';
 import { Image } from 'react-native';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 
 import logo from '../../assets/logo.png';
 import Background from '../../components/Background';
@@ -18,6 +18,8 @@ export default function SignUp({navigation}) {
   const [ name, setName] = useState('');
   const [ email, setEmail] = useState('');
   const [ password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit(){    
     dispatch(signUpRequest(name, email, password));
@@ -64,7 +66,7 @@ export default function SignUp({navigation}) {
             onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Criar conta</SubmitButton>          
+          <SubmitButton loading={loading} onPress={handleSubmit}>Criar conta</SubmitButton>          
         </Form>
 
         <SignLink onPress={()=> navigation.navigate('SignIn')}>
